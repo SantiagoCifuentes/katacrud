2f384f86595919ea9277a417d61a01dac0a88194
Santiago C <santiago.ci9619@gmail.com>
2022-02-22 23:43:42 -0500

    Se realizan algunos comentarios sobre lo que se entendió del código

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const initialState = {
 const Store = createContext(initialState);
 
 const Form = () => {
-  const formRef = useRef(null);
+  const formRef = useRef(null); //identifica las propiedades de un componente en especifico
 
-  const {dispatch, state: {item}} = useContext(Store);
-  const [state, setState] = useState({item});
+  const {dispatch, state: {item}} = useContext(Store);//el item que se llama acá es el del initial state
+  const [state, setState] = useState({item}); //previene que se utilice el submit si no se da click
 
 
   const onAdd = (event) => {
@@ -31,7 +31,7 @@ const Form = () => {
         "Content-type": "application/json"
       }
     })
-    .then(response => response.json())
+    .then(response => response.json()) //promesa que sirve para borrar los campos dentro del formulario
     .then((todo) => {
       dispatch({ type: "add-item", item: todo});
       setState({ name: ""});
@@ -40,7 +40,7 @@ const Form = () => {
   }
 
   const onEdit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); //previene que se utilice el submit si no se da click
 
     const request = {
       name: state.name,
@@ -55,7 +55,7 @@ const Form = () => {
         "Content-type": "application/json"
       }
     })
-    .then(response => response.json())
+    .then(response => response.json()) //promesa que sirve para borrar los campos dentro del formulario,lo actualiza
     .then((todo) => {
       dispatch({ type: "update-item", item: todo});
       setState({ name: ""});
@@ -66,7 +66,7 @@ const Form = () => {
   return <div className='container'>
     <form ref={formRef}>
     
-      <input type="text" name='name'  defaultValue={item.name} onChange={(event) => {
+      <input type="text" name='name'  defaultValue={item.name} onChange={(event) => { ////onChange sirve para que cada vez que el usuario  typee se agrega el nombre y  se guarda el estado
         setState({ ...state, name:event.target.value })
       }}/>
       {item.id && <button  className='btn btn-success' onClick={onEdit}>Actualizar</button>}
@@ -77,13 +77,13 @@ const Form = () => {
 
 const List = () => {
 
-  const {dispatch, state} = useContext(Store);
+  const {dispatch, state} = useContext(Store); //guarda los estados de la app
 
-  useEffect(() => {
+  useEffect(() => { //consulta de http, promesa
     fetch(HOST_API+"/todos")
     .then(response => response.json())
     .then((list) => {
-      dispatch({type: "update-list", list})
+      dispatch({type: "update-list", list})//el dispatch actualiza la lista
     })
   }, [state.list.length, dispatch]);
 
@@ -92,7 +92,7 @@ const List = () => {
       method: "DELETE"
     })
     .then((list) => {
-      dispatch({ type: "delete-item", id})
+      dispatch({ type: "delete-item", id}) //esta línea es como la condición, es decir que para que esta función entre en acción, tiene que haber una lista
     })
   };
 
@@ -129,7 +129,7 @@ const List = () => {
 function reducer(state, action) {
   switch (action.type) {
     case 'update-item':
-      const listUpdateEdit = state.list.map((item) => {
+      const listUpdateEdit = state.list.map((item) => { //itera los elementos y cuando exista el item que se está buscando, lo devuelve
         if (item.id === action.item.id){
           return action.item;
         }
@@ -157,7 +157,8 @@ function reducer(state, action) {
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  return <Store.Provider value={ {state, dispatch} }>
+  return <Store.Provider value={ {state, dispatch} //el dispatch es el método que permite enviar los cambios necesarios, como agregar o actualizar
+   }>
     {children}
   </Store.Provider>
 }
